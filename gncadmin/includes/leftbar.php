eedcb107a48888730370ccc850319cd8f1685570
Royan Harsha <64635379+CHUKzi@users.noreply.github.com>
2022-04-23 21:01:42 +0530

    Add files via upload

---
+++ gncadmin/includes/leftbar.php
@@ -0,0 +1,35 @@
+	<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+				<li><a href="main.php"><i class="fa fa-globe"></i> Main</a></li>
+				<li><a href="history.php"><i class="fa fa-history"></i> History</a></li>
+			<li class="ts-label"><u>Manage</u></li>
+			<li><a href="manage-banner.php"><i class="fa fa-tasks"></i>Manage Banner</a></li>
+			<li><a href="gallery-manage.php"><i class="fa fa-tasks"></i>Manage Gallery</a></li>
+			<li><a href="matches-manage.php"><i class="fa fa-tasks"></i>Manage Tournament</a></li>
+			<li><a href="blog-page.php"><i class="fa fa-newspaper-o"></i>Manage Blog Post</a></li>
+			<li><a href="sponsors-manage.php"><i class="fa fa-tasks"></i>Manage Sponsors</a></li>
+			<li class="ts-label"><u>pages</u></li>
+			<li><a href="#"><i class="fa fa-file"></i> Web Pages</a>
+				<ul>
+				<li><a href="home-page.php"><i class="fa fa-home"></i>Home Page</a></li>
+				<li><a href="about-us-page.php"><i class="fa fa-file"></i>About-us Page</a></li>
+				<li><a href="members-page.php"><i class="fa fa-file"></i>Members Page</a></li>
+				<li><a href="gallery-page.php"><i class="fa fa-file"></i>Gallery Page</a></li>
+				<li><a href="matches-page.php"><i class="fa fa-file"></i>Matches Page</a></li>
+				<li><a href="blogs-page.php"><i class="fa fa-file"></i>Blog Page</a></li>
+				<li><a href="services-page.php"><i class="fa fa-file"></i>Contact Page</a></li>
+				<!--<li><a href="404-page.php"><i class="fa fa-file"></i>404 Page</a></li>-->
+			</ul>
+			<li class="ts-label"><u>setting and other</u></li>
+			<li><a href="advanced.php"><i class="fa fa-tasks"></i>Advanced options</a></li>
+			</li>
+			</li>
+			<footer style="margin-top: 100px;">
+			<center><img src="img/admin.png"><p class="text-center" style="color:#ffffff;">&copy;<script>document.write(new Date().getFullYear());</script><br>DeV.C H U K z i</p></center>
+			</footer>
+		</nav>
+
+		
